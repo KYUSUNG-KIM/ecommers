0be1db60f19cf9f9744391f71bc5b94c810b6d58
KYUSUNG-KIM <goodks09@nate.com>
2024-03-08 04:52:18 +0900

    상품 재고 관련 메소드 추가
    - 상품 구매 가능 여부 체크
    - 상품 재고 감산 기능

--- src/main/java/com/toy/ecommerce/product/repository/ProductOptionRepository.java
+++ src/main/java/com/toy/ecommerce/product/repository/ProductOptionRepository.java
@@ -1,10 +1,15 @@
 package com.toy.ecommerce.product.repository;
 
-import com.toy.ecommerce.product.entity.Product;
+import com.toy.ecommerce.product.constants.SellStatus;
 import com.toy.ecommerce.product.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductOptionRepository extends JpaRepository<ProductOption, String> {
+
+    Optional<ProductOption> findByIdAndSellStatus(String optionCode, SellStatus sellStatus);
+
 }
---
+++ src/main/java/com/toy/ecommerce/user/dto/ChangePointForm.java
@@ -0,0 +1,20 @@
+package com.toy.ecommerce.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChangePointForm {
+
+    private String from;
+
+    private String message;
+
+    private int point;
+
+}
