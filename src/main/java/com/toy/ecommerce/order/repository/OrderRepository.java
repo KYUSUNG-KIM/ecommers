26527919e8f0a581627e1efb043daef0af08ba11
KYUSUNG-KIM <goodks09@nate.com>
2024-05-30 00:04:11 +0900

    src 패키지 삭제 - legacy 코드 삭제

--- src/main/java/com/toy/ecommerce/order/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.toy.ecommerce.order.repository;
-
-import com.toy.ecommerce.order.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
